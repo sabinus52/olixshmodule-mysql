24eb727fb9cee680016b0b617dbeb534fadeb16e
Olivier <sabinus52@gmail.com>
2015-05-01 20:44:59 +0200

    Correction avec la variable globale OLIX_MODULE_FILECONF

--- olixmod.sh
+++ olixmod.sh
@@ -78,7 +78,6 @@ olixmod_list()
 olixmod_init()
 {
     logger_debug "module_mysql__olixmod_init (null)"
-    source lib/stdin.lib.sh
     source modules/mysql/lib/mysql.lib.sh
     source modules/mysql/lib/action.lib.sh
     module_mysql_action_init $@
