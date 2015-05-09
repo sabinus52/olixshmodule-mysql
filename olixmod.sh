cc5c3b0faf9599527a4a3a6996b903611fba5a74
Olivier <sabinus52@gmail.com>
2015-05-09 18:10:57 +0200

    Modifications de commentaires

--- olixmod.sh
+++ olixmod.sh
@@ -1,16 +1,6 @@
 ###
 # Module de la gestion des bases MySQL
 # ==============================================================================
-# OLIX_MODULE_MYSQL_HOST            : Nom du serveur MySQL
-# OLIX_MODULE_MYSQL_PORT            : Numéro du port MySQL
-# OLIX_MODULE_MYSQL_USER            : Nom de l'utilisateur MySQL
-# OLIX_MODULE_MYSQL_PASS            : Mot de passe de l'utilisateur
-# OLIX_MODULE_MYSQL_BACKUP_DIR      : Emplacement des dumps lors de la sauvegarde
-# OLIX_MODULE_MYSQL_BACKUP_COMPRESS : Format de compression
-# OLIX_MODULE_MYSQL_BACKUP_PURGE    : Nombre de jours de retention de la sauvegarde
-# OLIX_MODULE_MYSQL_BACKUP_REPORT   : Format des rapports
-# OLIX_MODULE_MYSQL_BACKUP_EMAIL    : Email d'envoi de rapport
-# ------------------------------------------------------------------------------
 # @package olixsh
 # @module mysql
 # @author Olivier <sabinus52@gmail.com>
